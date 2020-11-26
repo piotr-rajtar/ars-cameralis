bda6b2e30955424e8c6da1772388b0a5e8e959ac
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-11-26 18:17:46 +0100

    Add members data in initial state

--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -1,6 +1,99 @@
 export const initialState = {
   members: {
-    data: {},
+    data: {
+      conductor: {
+        name: 'Dyrygent',
+        photo: 'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png',
+      },
+      soprano: [
+        {
+          name: 'Sopran #1',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Sopran #2',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Sopran #3',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Sopran #4',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Sopran #5',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Sopran #6',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+      ],
+      alto: [
+        {
+          name: 'Alt #1',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #2',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #3',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #4',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #5',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #6',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+        {
+          name: 'Alt #7',
+          photo: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/female-296989_960_720.png',
+        },
+      ],
+      tenor: [
+        {
+          name: 'Tenor #1',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+        {
+          name: 'Tenor #2',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+        {
+          name: 'Tenor #3',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+      ],
+      bass: [
+        {
+          name: 'Bas #1',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+        {
+          name: 'Bas #2',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+        {
+          name: 'Bas #3',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+        {
+          name: 'Bas #4',
+          photo: 'https://cdn.pixabay.com/photo/2014/04/03/10/44/person-311292_960_720.png',
+        },
+      ],
+    },
     loading: {
       active: false,
       error: false,
