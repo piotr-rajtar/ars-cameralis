d517585fe270124462919b583e2164bfd1785e2d
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-12-07 22:32:56 +0100

    Correct Contact view

--- client/src/components/views/Contact/Contact.js
+++ client/src/components/views/Contact/Contact.js
@@ -8,29 +8,159 @@ import clsx from 'clsx';
 
 import styles from './Contact.module.scss';
 
-const Component = ({ className, children }) => (
+import { PageTitle } from '../../common/PageTitle/PageTitle';
 
-  <div className={clsx(className, styles.root)}>
-    <h1 className={styles.title}>Napisz do nas!</h1>
+class Component extends React.Component {
 
-    <form className={styles.form}>
-      <input type='text' name='name' placeholder='Imię' required className={styles.input_field} />
-      <input type='text' name='surname' placeholder='Nazwisko' className={styles.input_field}/>
-      <input type='email' name='email' placeholder='Email' className={styles.input_field}/>
-      <textarea name='message' placeholder='Wiadomość' required maxLength={255} className={styles.message}/>
-      <button type='submit' value='submit' className={styles.button}>Wyślij</button>
-    </form>
+  state = {
+    formData: {
+      name: '',
+      surname: '',
+      email: '',
+      message: '',
+    },
+    snackbar: {
+      snackbarOpen: false,
+      snackbarMessage: '',
+      alertSeverity: '',
+    },
+  }
 
-  </div>
+  static propTypes = {
+    className: PropTypes.string,
+  }
 
-);
+  handleChange = (event) => {
+    const { id, value } = event.target;
+    const { formData } = this.state;
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+    this.setState({
+      formData: {
+        ...formData,
+        [id]: value,
+      },
+    });
+  }
 
+  formSubmit = (event) => {
+    event.preventDefault();
+    alert('wyslane');
 
+    this.clearForm();
+  }
+
+  clearForm = () => {
+
+    this.setState({
+      formData: {
+        name: '',
+        surname: '',
+        email: '',
+        message: '',
+      },
+    });
+  }
+
+  render() {
+    const { className } = this.props;
+    const { name, surname, email, message } = this.state.formData;
+
+    return(
+      <div className={clsx(className, styles.root)}>
+
+        <PageTitle>Napisz do nas!</PageTitle>
+
+        <form className={styles.form} autoComplete='off' onSubmit={this.formSubmit}>
+
+          <div className={styles.form_group_name}>
+            <label
+              htmlFor='name'
+              className={styles.label}
+            >
+              <span>Imię</span>
+            </label>
+
+            <label
+              htmlFor='surname'
+              className={styles.label}
+            >
+              <span>Nazwisko</span>
+            </label>
+
+            <input
+              type='text'
+              id='name'
+              name='imię'
+              required
+              className={styles.input_field_name}
+              value={name}
+              minLength={3}
+              maxLength={20}
+              onChange={this.handleChange}
+            />
+
+            <input
+              type='text'
+              id='surname'
+              name='nazwisko'
+              className={styles.input_field_name}
+              value={surname}
+              minLength={3}
+              maxLength={20}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className={styles.form_group}>
+            <label
+              htmlFor='email'
+              className={styles.label}
+            >
+              <span>Email</span>
+            </label>
+
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className={styles.input_field}
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className={styles.form_group}>
+            <label
+              htmlFor='message'
+              className={styles.label}
+            >
+              <span>Wiadomość ({message.length}/500)</span>
+            </label>
+
+            <textarea
+              id='message'
+              name='wiadomość'
+              required
+              maxLength={500}
+              className={styles.message}
+              value={message}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button
+            type='submit'
+            value='submit'
+            className={styles.button}
+          >
+            <span>Wyślij</span>
+          </button>
+        </form>
+
+      </div>
+    );
+  }
+}
 
 // const mapStateToProps = state => ({
 //   someProp: reduxSelector(state),
