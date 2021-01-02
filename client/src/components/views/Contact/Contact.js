2b93ad47dde909b06b801a51c34ab854e43a3333
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-01-02 14:29:11 +0100

    Use Form component

--- client/src/components/views/Contact/Contact.js
+++ client/src/components/views/Contact/Contact.js
@@ -9,6 +9,7 @@ import clsx from 'clsx';
 import styles from './Contact.module.scss';
 
 import { PageTitle } from '../../common/PageTitle/PageTitle';
+import { Form } from '../../features/Form/Form';
 
 import { emailParser } from '../../../utils';
 
@@ -135,95 +136,14 @@ class Component extends React.Component {
 
         <PageTitle>Napisz do nas!</PageTitle>
 
-        <form className={styles.form} autoComplete='off' onSubmit={this.formSubmit}>
-
-          <div className={styles.form_group_name}>
-            <label
-              htmlFor='name'
-              className={styles.label}
-            >
-              <span>Imię</span>
-            </label>
-
-            <label
-              htmlFor='surname'
-              className={styles.label}
-            >
-              <span>Nazwisko</span>
-            </label>
-
-            <input
-              type='text'
-              id='name'
-              name='imię'
-              required
-              className={styles.input_field_name}
-              value={name}
-              minLength={3}
-              maxLength={20}
-              onChange={this.handleChange}
-            />
-
-            <input
-              type='text'
-              id='surname'
-              name='nazwisko'
-              required
-              className={styles.input_field_name}
-              value={surname}
-              minLength={3}
-              maxLength={20}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className={styles.form_group}>
-            <label
-              htmlFor='email'
-              className={styles.label}
-            >
-              <span>Email</span>
-            </label>
-
-            <input
-              type='email'
-              id='email'
-              name='email'
-              required
-              className={styles.input_field}
-              value={email}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className={styles.form_group}>
-            <label
-              htmlFor='message'
-              className={styles.label}
-            >
-              <span>Wiadomość ({message.length}/500)</span>
-            </label>
-
-            <textarea
-              id='message'
-              name='wiadomość'
-              required
-              maxLength={500}
-              className={styles.message}
-              value={message}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <button
-            type='submit'
-            value='submit'
-            className={styles.button}
-          >
-            <span>Wyślij</span>
-          </button>
-        </form>
-
+        <Form
+          name={name}
+          surname={surname}
+          email={email}
+          message={message}
+          formSubmit={this.formSubmit}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
