20d7ba4551f7383383b6133534b8c1784dfdcdf0
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-05-04 22:28:25 +0200

    Add header container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header-container />
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import headerContainer from './components/headerContainer/headerContainer.vue';
+
+@Component({ components: { headerContainer } })
+export default class App extends Vue {}
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/headerContainer/headerContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>Header</header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class headerContainer extends Vue {}
+</script>
+
+<style lang="scss" module="style"></style>
