439046f9227b585988e831b88beda9ec622984e3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-06-18 19:15:52 +0200

    Add About view

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,22 +1,55 @@
 <template>
   <div :class="style.container">
-    <span>ABOUT</span>
+    <div :class="style.textContainer">
+      <h1 :class="style.header">O nas</h1>
+      <p :class="style.paragraph">{{ paragraph1 }}</p>
+      <p :class="style.paragraph">{{ paragraph2 }}</p>
+      <p :class="style.paragraph">{{ paragraph3 }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { paragraph1, paragraph2, paragraph3 } from './aboutContent';
 
 @Component({})
-export default class About extends Vue {}
+export default class About extends Vue {
+  paragraph1 = paragraph1;
+  paragraph2 = paragraph2;
+  paragraph3 = paragraph3;
+}
 </script>
 
 <style lang="scss" module="style">
 @import '../../scss/variables.scss';
 @import '../../scss/mixins.scss';
+@import '../../scss/media.scss';
 
 .container {
-  margin-top: 100px;
-  //background-image: url("paper.gif");
+  @include flex-centered;
+}
+
+.textContainer {
+  margin-top: 35 * $spacing-unit;
+  padding: 15 * $spacing-unit;
+  width: 80%;
+  max-height: 80vh;
+  background: $overlay;
+  color: $secondary-color;
+
+  @include screen-medium {
+    overflow-y: scroll;
+  }
+}
+
+.header {
+  text-align: center;
+  font-size: $font-size-title;
+}
+
+.paragraph {
+  margin-top: 5 * $spacing-unit;
+  font-size: $font-size-paragraph;
 }
 </style>
