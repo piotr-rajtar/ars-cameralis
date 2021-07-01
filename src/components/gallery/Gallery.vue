0248ee9aab4881e6c687adce99e2273ff10ef54c
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-07-01 19:38:55 +0200

    Amend gallery views

--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -1,22 +1,84 @@
 <template>
   <div :class="style.container">
-    <span>GALLERY</span>
+    <div :class="style.textContainer">
+      <h1 :class="style.header">Galeria</h1>
+      <div :class="style.tileContainer">
+        <div
+          v-for="(item, index) in galleryItems"
+          :key="index"
+          :class="style.tile"
+        >
+          {{ item }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { galleryItems } from './galleryContent';
 
 @Component({})
-export default class Gallery extends Vue {}
+export default class Offer extends Vue {
+  galleryItems = galleryItems;
+}
 </script>
 
 <style lang="scss" module="style">
 @import '../../scss/variables.scss';
 @import '../../scss/mixins.scss';
+@import '../../scss/media.scss';
 
 .container {
-  margin-top: 100px;
-  //background-image: url("paper.gif");
+  @include flex-centered;
+}
+
+.textContainer {
+  margin: 35 * $spacing-unit 0 30 * $spacing-unit;
+  padding: 15 * $spacing-unit;
+  width: 80%;
+  max-height: 80vh;
+  background: $overlay;
+  color: $secondary-color;
+  overflow-y: auto;
+
+  @include screen-medium {
+    padding: 5 * $spacing-unit;
+    max-height: 70vh;
+  }
+}
+
+.header {
+  text-align: center;
+  font-size: $font-size-title;
+  margin-bottom: 15 * $spacing-unit;
+
+  @include screen-medium {
+    margin-bottom: 5 * $spacing-unit;
+  }
+}
+
+.tileContainer {
+  @include flex-tile;
+}
+
+.tile {
+  @include flex-centered;
+  width: 300px;
+  height: 300px;
+  border-radius: 2 * $spacing-unit;
+  border: 2 * $spacing-unit solid $secondary-color;
+  font-size: $font-size-title-medium;
+  margin: 10 * $spacing-unit;
+  cursor: pointer;
+
+  &:hover {
+    background-color: $secondary-color;
+    opacity: 0.8;
+    color: $main-color;
+    transition: 1s ease;
+    transform: scale(1.5);
+  }
 }
 </style>
---
+++ src/components/gallery/galleryContent.ts
@@ -0,0 +1,12 @@
+export const galleryItems: Array<string> = [
+  'Zdjęcie 1',
+  'Zdjęcie 2',
+  'Zdjęcie 3',
+  'Zdjęcie 4',
+  'Zdjęcie 5',
+  'Zdjęcie 6',
+  'Zdjęcie 7',
+  'Zdjęcie 8',
+  'Zdjęcie 9',
+  'Zdjęcie 10',
+];
