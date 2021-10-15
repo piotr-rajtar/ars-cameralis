88051d9f496f76025ed719094664ce4f95b5f007
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-10-15 21:26:47 +0200

    Fix mobile background

--- src/App.vue
+++ src/App.vue
@@ -38,9 +38,11 @@ export default class App extends Vue {
   flex-direction: column;
 
   @include screen-mobile {
-    background-color: contain, no-repeat $main-color;
+    background-color: $main-color;
     background-image: url('/images/background_mobile.avif'),
       url('/images/background_mobile_f.webp');
+    background-size: contain;
+    background-repeat: no-repeat;
   }
 }
 
