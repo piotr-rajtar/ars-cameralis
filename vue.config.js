4e1c53e298142f7a53f5ebf056483f8924979da3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-11-16 18:40:13 +0100

    Add router paths and config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { paths } = require('./paths.ts');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
 module.exports = {
   css: {
     requireModuleExtension: true,
@@ -9,5 +13,10 @@ module.exports = {
         localsConvention: 'camelCaseOnly',
       },
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({ base: 'https://ars-cameralis.netlify.app', paths }),
+    ],
   },
 };
